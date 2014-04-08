23feaa50236ac1d2498b3a5ddb031d4993ac8f5d
Sven Reglitzki <sven.reglitzki@flavia-it.de>
2014-04-08 22:50:48 +0200

    added customer dummies and a resource. Populating table with dummy data

--- app/scripts/controllers/customerGridCtrl.js
+++ app/scripts/controllers/customerGridCtrl.js
@@ -1,19 +1,4 @@
 angular.module('grid')
-	.controller('customerGridCtrl', function($scope) {
-		$scope.customers = [
-			{
-				firstname: 'Annakin',
-				lastname: 'Skywalker',
-				phone: '06215/32434235',
-				email: 'as@death-star.org',
-				address: 'Tatooine'
-			},
-			{
-				firstname: 'Annakin',
-				lastname: 'Skywalker',
-				phone: '06215/32434235',
-				email: 'as@death-star.org',
-				address: 'Tatooine'
-			}
-		]
+	.controller('customerGridCtrl', function($scope, Customer) {
+		$scope.customers = Customer.query();
 	});
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var express = require('express');
+var _ = require('underscore');
 
 var app = express();
+
+app.get('/customers', function(req, res) {
+	res.send(_.range(50).map(function(idx) {
+		return {
+			id: idx + 1,
+			firstname: 'Annakin',
+			lastname: 'Skywalker',
+			phone: '06215/32434235',
+			email: 'as@death-star.org',
+			address: 'Tatooine'
+		}
+	}));
+});
 
 app.get('/', function(req, res) {
 	res.sendfile('app/index.html');
