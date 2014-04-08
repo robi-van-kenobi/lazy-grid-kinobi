fc4c9c5e02d087cd85bf30077d599941c6314423
Sven Reglitzki <sven.reglitzki@flavia-it.de>
2014-04-09 00:01:31 +0200

    implemented client-side pagination with 50 items per page

--- app/scripts/controllers/customerGridCtrl.js
+++ app/scripts/controllers/customerGridCtrl.js
@@ -1,4 +1,17 @@
 angular.module('grid')
 	.controller('customerGridCtrl', function($scope, Customer) {
-		$scope.customers = Customer.query();
+		$scope.page = 1;
+		$scope.loadPage = function(page) {
+			$scope.customers = Customer.query({
+				page: page
+			}, function(data) {
+				$scope.totalCount = data.totalCount;
+				$scope.customers = data.customers;
+				$scope.start = (page - 1) * 50 + 1;
+				$scope.end = data.totalCount < page * 50 ? data.totalCount : page * 50;
+				$scope.page = page;
+				$scope.pages = _.range(Math.ceil(data.totalCount/50));
+			});
+		};
+		$scope.loadPage($scope.page);
 	});
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var _ = require('underscore');
+var url = require('url');
 
 var app = express();
 
 app.get('/customers', function(req, res) {
-	res.send(_.range(50).map(function(idx) {
-		return {
-			id: idx + 1,
-			firstname: 'Annakin',
-			lastname: 'Skywalker',
-			phone: '06215/32434235',
-			email: 'as@death-star.org',
-			address: 'Tatooine'
-		}
-	}));
+	res.send({
+		totalCount: 201,
+		customers: _.range(50).map(function(idx) {
+			return {
+				id: (req.query.page - 1) * 50 + idx + 1,
+				firstname: 'Annakin',
+				lastname: 'Skywalker',
+				phone: '06215/32434235',
+				email: 'as@death-star.org',
+				address: 'Tatooine'
+			};
+		})
+	});
 });
 
 app.get('/', function(req, res) {
